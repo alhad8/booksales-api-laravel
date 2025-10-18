6878162c7d102bd4634eb16e27be384f28c62955
had8 <alhadidadityaabidin@gmail.com>
2025-10-18 22:07:41 +0700

    Pertemuan 3 Laravel FIXED

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,12 +18,10 @@ class RouteServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->routes(function () {
-            // Route API dengan prefix /api
             Route::middleware('api')
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
-            // Route WEB
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
--- routes/api.php
+++ routes/api.php
@@ -5,27 +5,16 @@ use App\Http\Controllers\BookController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\GenreController;
 
-// Test route
-Route::get('/test', function () {
-    return response()->json([
-        'success' => true,
-        'message' => 'API route works!'
-    ]);
-});
-
-// Books API
 Route::prefix('books')->group(function () {
     Route::get('/', [BookController::class, 'index']);
     Route::post('/', [BookController::class, 'store']);
     Route::get('/{id}', [BookController::class, 'show']);
 });
 
-// Authors API
 Route::prefix('authors')->group(function () {
     Route::get('/', [AuthorController::class, 'index']);
 });
 
-// Genres API
 Route::prefix('genres')->group(function () {
     Route::get('/', [GenreController::class, 'index']);
 });
