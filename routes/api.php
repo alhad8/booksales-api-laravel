148bcb643773fe64c6cb8a28ef0360e7c69eb41c
had8 <alhadidadityaabidin@gmail.com>
2025-10-20 21:56:54 +0700

    SHOW, UPDATE, DESTROY DATA Pertemuan 5

--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,8 @@ use App\Http\Controllers\BookController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\GenreController;
 
-// API Resource Routes untuk Books
 Route::apiResource('books', BookController::class);
 
-// API Resource Routes untuk Authors
 Route::apiResource('authors', AuthorController::class);
 
-// API Resource Routes untuk Genres
 Route::apiResource('genres', GenreController::class);
